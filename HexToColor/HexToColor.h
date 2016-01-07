42b42c68844687f9a81207f07e20978d060b60bf
Gareth Jones <gareth@garethpaul.com>
2016-01-07 09:11:18 -0800

    adding initial

---
+++ HexToColor/HexToColor.h
@@ -0,0 +1,19 @@
+//
+//  HexToColor.h
+//  HexToColor
+//
+//  Created by Gareth Jones on 1/7/16.
+//  Copyright © 2016 GPJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HexToColor.
+FOUNDATION_EXPORT double HexToColorVersionNumber;
+
+//! Project version string for HexToColor.
+FOUNDATION_EXPORT const unsigned char HexToColorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HexToColor/PublicHeader.h>
+
+

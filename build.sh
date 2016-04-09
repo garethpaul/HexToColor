f545ecc3927a3bb5022bd0b20da53e9fdb220c5d
Gareth Jones <gareth@garethpaul.com>
2016-04-08 20:00:00 -0600

    adding build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+function ci_lib() {
+    NAME=$1
+    xcodebuild -project HexToColor.xcodeproj \
+               -scheme "HexToColorTests" \
+               -destination "platform=iOS Simulator,name=${NAME}" \
+               -sdk iphonesimulator \
+               build test
+}
+ci_lib "iPhone 5"
